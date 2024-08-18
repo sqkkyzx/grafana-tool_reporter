3a60f9b465ae968f72267aab0788feeb14769381
YiZixuan <admin@sqkkzyx.com>
2024-08-19 03:30:19 +0800

    设置发送器基类

---
+++ notifier/base.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class File(ABC):
+    title: str
+    filetype: str
+    filepath: str
+    fileurl: str
+    viewurl: str
+    slug: str
+
+
+class BaseNotifier(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def send(self, file: File, receiver: List[str]):
+        pass
